4a2571830ede7273e715617fd0ffb574d1904fbb
yuechenw213 <yuechenw@andrew.cmu.edu>
2022-04-11 01:15:31 -0400

    tic tac toe game with neutralinojs

--- myapp/resources/js/main.js
+++ myapp/resources/js/main.js
@@ -1,63 +1,134 @@
-// This is just a sample app. You can structure your Neutralinojs app code as you wish.
-// This example app is written with vanilla JavaScript and HTML.
-// Feel free to use any frontend framework you like :)
-// See more details: https://neutralino.js.org/docs/how-to/use-a-frontend-library
 
-function showInfo() {
-    document.getElementById('info').innerHTML = `
-        ${NL_APPID} is running on port ${NL_PORT}  inside ${NL_OS}
-        <br/><br/>
-        <span>server: v${NL_VERSION} . client: v${NL_CVERSION}</span>
-        `;
+let currentPlayer = 'X';
+let getUsername = async () => {
+    
+    window.addEventListener('DOMContentLoaded', () => {
+        const tiles = Array.from(document.querySelectorAll('.tile'));
+        const playerDisplay = document.querySelector('.display-player');
+        const resetButton = document.querySelector('#reset');
+        const announcer = document.getElementById('name');
+    
+        let board = ['', '', '', '', '', '', '', '', ''];
+        
+        let isGameActive = true;
+    
+        const PLAYERX_WON = 'PLAYERX_WON';
+        const PLAYERO_WON = 'PLAYERO_WON';
+        const TIE = 'TIE';
+    
+    
+        /*
+            Indexes within the board
+            [0] [1] [2]
+            [3] [4] [5]
+            [6] [7] [8]
+        */
+    
+        const winningConditions = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+    
+        function handleResultValidation() {
+            let roundWon = false;
+            for (let i = 0; i <= 7; i++) {
+                const winCondition = winningConditions[i];
+                const a = board[winCondition[0]];
+                const b = board[winCondition[1]];
+                const c = board[winCondition[2]];
+                if (a === '' || b === '' || c === '') {
+                    continue;
+                }
+                if (a === b && b === c) {
+                    roundWon = true;
+                    break;
+                }
+            }
+    
+        if (roundWon) {
+                announce(currentPlayer === 'X' ? PLAYERX_WON : PLAYERO_WON);
+                isGameActive = false;
+                return;
+            }
+    
+        if (!board.includes(''))
+            announce(TIE);
+        }
+    
+        const announce = (type) => {
+            switch(type){
+                case PLAYERO_WON:
+                    announcer.innerHTML = 'Player O Won';
+                    break;
+                case PLAYERX_WON:
+                    announcer.innerHTML = 'Player X Won';
+                    break;
+                case TIE:
+                    announcer.innerText = 'Tie';
+            }
+            announcer.classList.remove('hide');
+        };
+    
+        const isValidAction = (tile) => {
+            if (tile.innerText === 'X' || tile.innerText === 'O'){
+                return false;
+            }
+    
+            return true;
+        };
+    
+        const updateBoard =  (index) => {
+            board[index] = currentPlayer;
+        }
+    
+        const changePlayer = () => {
+            // playerDisplay.classList.remove(`player${currentPlayer}`);
+            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+            if(isGameActive) {
+            announcer.innerText ="Player "+ currentPlayer + "'s turn";
+            }
+            // playerDisplay.classList.add(`player${currentPlayer}`);
+        }
+    
+        const userAction = (tile, index) => {
+            if(isValidAction(tile) && isGameActive) {
+                tile.innerText = currentPlayer;
+                tile.classList.add(`player${currentPlayer}`);
+                updateBoard(index);
+                handleResultValidation();
+                changePlayer();
+            }
+        }
+        
+        const resetBoard = () => {
+            board = ['', '', '', '', '', '', '', '', ''];
+            isGameActive = true;
+            announcer.classList.add('hide');
+    
+            if (currentPlayer === 'O') {
+                changePlayer();
+            }
+    
+            tiles.forEach(tile => {
+                tile.innerText = '';
+                tile.classList.remove('playerX');
+                tile.classList.remove('playerO');
+            });
+        }
+    
+        tiles.forEach( (tile, index) => {
+            tile.addEventListener('click', () => userAction(tile, index));
+        });
+    
+        resetButton.addEventListener('click', resetBoard);
+    });
 }
-
-function openDocs() {
-    Neutralino.os.open("https://neutralino.js.org/docs");
-}
-
-function openTutorial() {
-    Neutralino.os.open("https://www.youtube.com/watch?v=txDlNNsgSh8&list=PLvTbqpiPhQRb2xNQlwMs0uVV0IN8N-pKj");
-}
-
-function setTray() {
-    if(NL_MODE != "window") {
-        console.log("INFO: Tray menu is only available in the window mode.");
-        return;
-    }
-    let tray = {
-        icon: "/resources/icons/trayIcon.png",
-        menuItems: [
-            {id: "VERSION", text: "Get version"},
-            {id: "SEP", text: "-"},
-            {id: "QUIT", text: "Quit"}
-        ]
-    };
-    Neutralino.os.setTray(tray);
-}
-
-function onTrayMenuItemClicked(event) {
-    switch(event.detail.id) {
-        case "VERSION":
-            Neutralino.os.showMessageBox("Version information",
-                `Neutralinojs server: v${NL_VERSION} | Neutralinojs client: v${NL_CVERSION}`);
-            break;
-        case "QUIT":
-            Neutralino.app.exit();
-            break;
-    }
-}
-
-function onWindowClose() {
-    Neutralino.app.exit();
-}
-
-Neutralino.init();
-
-Neutralino.events.on("trayMenuItemClicked", onTrayMenuItemClicked);
-Neutralino.events.on("windowClose", onWindowClose);
-
-if(NL_OS != "Darwin") { // TODO: Fix https://github.com/neutralinojs/neutralinojs/issues/615
-    setTray();
-}
-
-showInfo();
+  
+  Neutralino.init();
+  getUsername();
